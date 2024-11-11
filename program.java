9817b76615f3cb370eb2401e84b785fc67511114
Muhammad-Saud631 <muhammadsaud226688@gmail.com>
2024-11-11 22:39:41 +0500

    Delete program.java

--- program.java
+++
@@ -1,7 +0,0 @@
-import java.util.LinkedList;
-import java.util.Queue;
-public class main
-    public static void main(String[] args) {
-        System.out.print("Hello!")
-        System.out.print("Welcome To Github!")
-       
